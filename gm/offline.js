13d252663d3d8de32580b0ccf78c2fd0a6c0cc12
oliwerton <50372229+oliwerton@users.noreply.github.com>
2019-05-08 10:57:19 -0300

    Add files via upload

---
+++ gm/offline.js
@@ -0,0 +1,1 @@
+{"version":1557323127960,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/shared-0-sheet0.png","images/shared-0-sheet1.png","images/shared-0-sheet2.png","images/shared-0-sheet3.png","icons/icon-16.png","icons/icon-32.png","icons/icon-114.png","icons/icon-128.png","icons/icon-256.png","icons/loading-logo.png","register-sw.js"]}
